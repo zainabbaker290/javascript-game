af8168546040cea50852764b2d223365e52465fc
Zainab Baker <119340513@umail.ucc.ie>
2021-04-05 15:44:36 +0100

    changed bullet function, added isDead to object

--- Javascript/sprites.js
+++ Javascript/sprites.js
@@ -11,8 +11,6 @@ let backgroundImage = new Image();
 let tilesPerRow = 6;
 let tileSize = 16;
 
-
-
 let background = [
     [22, 22, 21, 20, 19, 18, 22, 21, 20, 19, 18, 22, 21, 20, 19, 18, 22, 21, 20, 19, 18, 30, 31, 32, 33, 34, 35, 22, 21, 20, 19, 18, 22, 21, 20, 19, 18, 22, 21, 20, 19, 18, 22, 21, 20, 19, 18, 22, 21, 20, 19, 18, 22, 21, 20, 19, 18, 22, 21, 20, 19, 18, 22, 21],
     [28, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 36, 37, 38, 39, 40, 41, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27],
@@ -56,8 +54,6 @@ let background = [
     [28, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 36, 37, 38, 39, 40, 41, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27, 26, 25, 24, 28, 27],
 ]
 
-
-
 let player = {
     x: 0,
     y: 0,
@@ -76,9 +72,10 @@ let enemy = {
     height: 48,
     frameX: 0,
     frameY: 0,
-    xChange: 3,
+    xChange: 0,
     yChange: 0,
-}
+    isDead: false,
+};
 
 let bullet = {
     x: 0,
@@ -95,7 +92,6 @@ let bullet = {
 let playerImage = new Image();
 let enemyImage = new Image();
 let bulletImage = new Image();
-
 
 let floor;
 
@@ -121,11 +117,10 @@ function init() {
     player.y = player.height;
     enemy.x = canvas.width / 2;
     enemy.y = floor - player.height;
- 
+    
 
     window.addEventListener("keydown", activate, false);
     window.addEventListener("keyup", deactivate, false);
-
 
     draw();
 }
@@ -160,7 +155,7 @@ function draw() {
     context.drawImage(playerImage,
         player.width * player.frameX, player.height * player.frameY, player.width, player.height,
         player.x, player.y, player.width, player.height);
-
+    
     if ((moveLeft || moveRight) && !(moveLeft && moveRight)) {
         player.frameX = (player.frameX + 1) % 4;
     }
@@ -172,13 +167,17 @@ function draw() {
         context.drawImage(bulletImage,
             bullet.width * bullet.frameX, bullet.height * bullet.frameY, bullet.width, bullet.height,
             bullet.x, bullet.y, bullet.width, bullet.height);
-
     }
 
-    // Draw enemy
-    context.drawImage(enemyImage,
-        enemy.width * enemy.frameX, enemy.height * enemy.frameY, enemy.width, enemy.height,
-        enemy.x, enemy.y, enemy.width, enemy.height);
+    if (enemy_collides(bullet)) {
+        enemy.isDead = true;  
+    } 
+
+    if (!enemy.isDead) {
+        context.drawImage(enemyImage,
+            enemy.width * enemy.frameX, enemy.height * enemy.frameY, enemy.width, enemy.height,
+            enemy.x, enemy.y, enemy.width, enemy.height);      
+    }
 
     // Handle key presses.
     if (moveLeft) {
@@ -192,13 +191,11 @@ function draw() {
     if ((moveUp) && !(moveUp && moveRight) && !(moveUp && moveLeft)) {
         player.yChange = player.yChange - 0.5;
         player.frameY = 3;
-
     }
     if ((moveDown) && !(moveDown && moveRight) && !(moveDown && moveLeft)) {
         player.yChange = player.yChange + 0.5;
         player.frameY = 0;
     }
-
     if (spaceBar) {
         if (!bullet_on_screen(bullet)) {
             bullet.y = player.y;
@@ -220,14 +217,8 @@ function draw() {
                 bullet.yChange = -8;
                 bullet.xChange = 0
             }
-
         }
-
-
     }
-
-
-
 
     // Update the player.
     player.x = player.x + player.xChange;
@@ -240,7 +231,6 @@ function draw() {
     //Update bullet 
     bullet.x = bullet.x + bullet.xChange;
     bullet.y = bullet.y + bullet.yChange;
-
 
     //enemy frames
     if (enemy) {
@@ -255,15 +245,11 @@ function draw() {
             enemy.xChange = -enemy.xChange;
             enemy.frameY = 1;
         }
-
-
     }
-
 
     // Physics.
     player.xChange = player.xChange * 0.9; // Friction!
     player.yChange = player.yChange * 0.9; // Friction!
-
 
     // Going off left or right.
     if (player.x + player.width < 0) {
@@ -279,23 +265,11 @@ function draw() {
         player.y = -player.height;
     }
 
-
-    // if the bullet collides with the enemy 
-    // moved enemy to area off canvas 
-    if (enemy_collides(bullet)) {
-        context.drawImage(enemyImage,
-            enemy.x = 1050, enemy.y = 1050);
-    }
-
     if (player_collides(enemy)) {
         stop("You lose");
-        return;
-    
+        return;   
     }
 }
-
-
-
 
 function activate(event) {
     let key = event.key;
@@ -325,9 +299,7 @@ function deactivate(event) {
     } else if (key === " ") {
         spaceBar = false;
     }
-
 }
-
 
 // if bullet is on the screen
 function bullet_on_screen(bullet) {
@@ -337,14 +309,16 @@ function bullet_on_screen(bullet) {
 
 // enemy colliding with bullet
 function enemy_collides(bullet) {
-    if (enemy.x + enemy.width < bullet.x ||
-        bullet.x + bullet.width < enemy.x ||
-        enemy.y > bullet.height + bullet.y ||
-        bullet.y > enemy.height + enemy.y) {
-        return false;
-    } else {
-        return true;
-    }
+    let bulletStartX = bullet.x;
+    let bulletEndX = bullet.x + bullet.width;
+    let enemyStartX = enemy.x;
+    let enemyEndX = enemy.x + enemy.width; 
+    let bulletStartY = bullet.y;
+    let bulletEndY = bullet.y + bullet.height;
+    let enemyStartY = enemy.y;
+    let enemyEndY = enemy.y + enemy.height; 
+    return (((bulletStartX >= enemyStartX) || (bulletEndX >= enemyStartX)) && ((bulletEndX <= enemyEndX) || (bulletStartX <= enemyEndX))) &&
+            (((bulletStartY >= enemyStartY) || (bulletEndY >= enemyStartY)) && ((bulletEndY <= enemyEndY) || (bulletStartY <= enemyEndY)) )
 }
 
 // enemy and the player collide
@@ -359,8 +333,6 @@ function player_collides(enemy) {
     }
 }
 
-
-
 // stop the game 
 function stop(outcome) {
     window.removeEventListener("keydown", activate, false);
@@ -370,5 +342,3 @@ function stop(outcome) {
     outcome_element.innerHTML = outcome;
 
 }
-
-
